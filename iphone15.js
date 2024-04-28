430801dbbdd494c4e40ae35279f2220b309251dd
Adam-Fawwaz <168169843+Adam-Fawwaz@users.noreply.github.com>
2024-04-28 12:39:48 +0300

    Add files via upload

---
+++ iphone15.js
@@ -0,0 +1,216 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("iphone15");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "iphone15.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("iphone15pro");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "product.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("15promax");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "15promax.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("14");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "14.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("14pro");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "14pro.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("14promax");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "14promax.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("13");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "13.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("13pro");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "13pro.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("S23");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "s23.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("s24u");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "s24u.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A54");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A54.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A55");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A55.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A047");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A047.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A24");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A24.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A25");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A25.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("A15");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "A15.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("P2");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "P2.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Enjoy70z");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Enjoy70z.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Y72");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Y72.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("12ultra");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "12ultra.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Enjoy70");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Enjoy70.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Nova12");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Nova12.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Nova12pro");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Nova12pro.html";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var photo = document.getElementById("Nova12lite");
+
+    photo.addEventListener("click", function() {
+
+        window.location.href = "Nova12lite.html";
+    });
+});
